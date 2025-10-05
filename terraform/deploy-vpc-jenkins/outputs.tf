0fe4ac3000568f39defc01e92b3dc392971e260d
TarasAndreiko <TARAS.ANDREIKO@lnu.edu.ua>
2025-10-06 02:57:00 +0300

    HP-53-write terraform to build ec2 machine with jenkins and additional tools

---
+++ terraform/deploy-vpc-jenkins/outputs.tf
@@ -0,0 +1,26 @@
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+
+output "jenkins_sg_id" {
+  value = aws_security_group.jenkins_sg.id
+}
+
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id,
+    aws_subnet.private_3.id,
+    aws_subnet.private_4.id
+  ]
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet for Jenkins"
+  value       = aws_subnet.public.id
+}
